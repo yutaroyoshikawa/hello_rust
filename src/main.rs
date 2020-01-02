2aca34f01061e6c377fe0ed613baf4d22b123cd2
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-01-02 23:58:54 +0900

    食事する哲学者

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::thread;
+use std::time::Duration;
+
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+
+    fn eat(&self) {
+        println!("{} is eating.", self.name);
+
+        thread::sleep(Duration::from_millis(1000));
+
+        println!("{} is done eating.", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+
+    for p in &philosophers {
+        p.eat();
+    }
 }
